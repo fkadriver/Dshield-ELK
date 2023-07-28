2fc57af6769cd2c12ec04a16fea18d854447d91f
Scott Jensen <fkadriver@gmail.com>
2023-07-27 20:30:32 -0500

    modified:   .env 	modified:   docker-compose.yml 	deleted:    elasticsearch/scripts/cowrie-setup.sh 	renamed:    elasticsearch/scripts/cowrie-dshield-index.json -> scripts/cowrie-dshield-index.json 	renamed:    elasticsearch/scripts/cowrie-dshield-policy.json -> scripts/cowrie-dshield-policy.json 	renamed:    elasticsearch/scripts/cowrie-index.json -> scripts/cowrie-index.json 	renamed:    elasticsearch/scripts/cowrie-policy.json -> scripts/cowrie-policy.json 	new file:   scripts/cowrie-setup.sh 	renamed:    elasticsearch/scripts/cowrie-sqlite-index.json -> scripts/cowrie-sqlite-index.json 	renamed:    elasticsearch/scripts/cowrie-sqlite-policy.json -> scripts/cowrie-sqlite-policy.json 	new file:   scripts/cowrie-template.ndjson 	new file:   scripts/dshield_sensor_8.71.ndjson 	new file:   scripts/export.ndjson 	new file:   scripts/file.ndjson 	new file:   scripts/output.json 	new file:   scripts/test.ndjson

---
+++ scripts/cowrie-setup.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+echo "Setting up environment variables"
+export scriptdir='/usr/share/elasticsearch/scripts'
+echo $scriptdir
+export curlcmd='curl --cacert /usr/share/elasticsearch/config/certs/ca/ca.crt -u elastic:'$ELASTIC_PASSWORD
+echo $curlcmd
+#$curlcmd -H 'Content-Type: application/json' -XGET https://es01:9200/_cluster/health?pretty
+
+echo "changing directory"
+cd $scriptdir
+
+# cowrie setup
+echo "Setting up cowrie"
+$curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_ilm/policy/cowrie --data-binary @cowrie-policy.json; echo
+$curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_index_template/cowrie --data-binary @cowrie-index.json; echo
+
+# cowrie-dshield setup
+echo "Setting up cowrie.dshield"
+$curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_ilm/policy/cowrie.dshield  --data-binary @cowrie-dshield-policy.json; echo
+$curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_index_template/cowrie.dshield --data-binary @cowrie-dshield-index.json; echo
+
+# cowrie-sqlite setup
+echo "Setting up cowrie.sqlite"
+$curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_ilm/policy/cowrie.sqlite --data-binary @cowrie-sqlite-policy.json; echo
+$curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_index_template/cowrie.sqlite --data-binary @cowrie-sqlite-index.json; echo
+
+# # dashboard setup
+# echo "Setting up dshield_sensor"
+# #$curlcmd -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_bulk?pretty --data-binary @dshield_sensor_8.7.1.ndjson
+# curl -u kibana_system:$KIBANA_PASSWORD -X POST "es01:5601/api/saved_objects/_import -H "kbn-xsrf: true" --form file=@dshield_sensor_8.71.ndjson" -H 'kbn-xsrf: true'
+
