c9173c824d75640c40d2f65e94ee03c37a85ce95
Scott Jensen <fkadriver@gmail.com>
2023-08-01 21:53:39 -0500

    testing cowrie script 	modified:   docker-compose.yml 	modified:   logstash/logstash.yml 	new file:   logstash/pipeline/logstash-100-input.conf 	modified:   scripts/cowrie-setup.sh

--- scripts/cowrie-setup.sh
+++ scripts/cowrie-setup.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 echo "Setting up environment variables"
-export scriptdir='/usr/share/elasticsearch/scripts'
+export scriptdir='/usr/share/scripts'
 echo $scriptdir
-export curlcmd='curl --cacert /usr/share/elasticsearch/config/certs/ca/ca.crt -u elastic:'$ELASTIC_PASSWORD
+export curlcmd='curl --cacert /usr/share/config/certs/ca/ca.crt -u elastic:'$ELASTIC_PASSWORD
 echo $curlcmd
 #$curlcmd -H 'Content-Type: application/json' -XGET https://es01:9200/_cluster/health?pretty
 
@@ -26,7 +26,8 @@ $curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_ilm
 $curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_index_template/cowrie.sqlite --data-binary @cowrie-sqlite-index.json; echo
 
 # # dashboard setup
-# echo "Setting up dshield_sensor"
+echo "Setting up dshield_sensor"
 # #$curlcmd -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_bulk?pretty --data-binary @dshield_sensor_8.7.1.ndjson
-# curl -u kibana_system:$KIBANA_PASSWORD -X POST "es01:5601/api/saved_objects/_import -H "kbn-xsrf: true" --form file=@dshield_sensor_8.71.ndjson" -H 'kbn-xsrf: true'
+curl -v --cacert /usr/share/config/certs/ca/ca.crt -u kibana_system:$KIBANA_PASSWORD -X POST https://es01:5601/api/saved_objects/_import -H "kbn-xsrf: true" --form file=@dshield_sensor_8.71.ndjson -H 'kbn-xsrf: true';echo
+# curl -u kibana_system:changeme  -X POST localhost:5601/api/saved_objects/_import -H "kbn-xsrf: true" --form file=@dshield_sensor_8.71.ndjson -H 'kbn-xsrf: true'
 
